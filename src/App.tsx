3187ba93b4b26d144a8a863cab8310f380e4e22c
koog7 <k.ognev709@mail.com>
2024-05-15 15:56:52 +0600

    НапиÑсана логика генерации чисел в компоненте

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css'
+import RandomNumberLink from './Components/RandomNumber.tsx';
 
 function App() {
 
   return (
-    <>
-        <a className="codepen-button"><span>Сгенерировать числа</span></a>
-    </>
+      <>
+          <div className="App">
+              <header className="App-header">
+                  <h1>Random Number Generator</h1>
+                  <RandomNumberLink/>
+              </header>
+          </div>
+      </>
   )
 }
 
--- src/Components/RandomNumber.tsx
+++ src/Components/RandomNumber.tsx
@@ -1,1 +1,34 @@
+import React, {useState} from 'react';
+import '../App.css'
+const RandomNumberLink: React.FC = () => {
+    const [numArray, setNumArray] = useState<number[]>([]);
+    const generateRandomNumber = () => {
+        const newNumArray: number[] = [];
+        while (newNumArray.length < 5) {
+            const randomNumber = Math.floor(Math.random() * (36 - 5 + 1)) + 5;
+            if (!newNumArray.includes(randomNumber)) {
+                newNumArray.push(randomNumber);
+            }
+        }
+        setNumArray(newNumArray);
+    };
+
+    return (
+        <div>
+            <ul>
+                <a className={'codepen-button'} href="#" onClick={(e) => {
+                    e.preventDefault();
+                    generateRandomNumber();
+                }}>
+                    <span>Сгенерировать числа</span>
+                </a>
+                {numArray.slice().sort((a, b) => a - b).map((num, index) => (
+                    <a className={'circle'} key={index}>{num} </a>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default RandomNumberLink;
 
